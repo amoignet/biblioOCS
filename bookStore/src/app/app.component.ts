0aff18424068549d43b0026959f191d4999be487
amoignet <antoinemoignet@gmail.com>
2020-09-10 17:48:12 -0400

    create signUp, signIn, signOut, guards

--- bookStore/src/app/app.component.ts
+++ bookStore/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookStore';
+constructor() {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyDGrLHnJGQtDfHLwkxKp450CdurE8AYmQw',
+    authDomain: 'bookstrore-49cd6.firebaseapp.com',
+    databaseURL: 'https://bookstrore-49cd6.firebaseio.com',
+    projectId: 'bookstrore-49cd6',
+    storageBucket: 'bookstrore-49cd6.appspot.com',
+    messagingSenderId: '837513602585',
+    appId: '1:837513602585:web:d3f1e78da2f3017da56c88',
+    measurementId: 'G-XQHMYGHHNJ'
+  };
+  firebase.initializeApp(firebaseConfig);
+
+}
+
 }
